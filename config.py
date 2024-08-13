21418fedd5f57f95da480dae5690dcca41f40a1b
arindam69x <147473276+arindam69x@users.noreply.github.com>
2024-08-14 00:19:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/arindam69x/New-music-bot69",
+    "https://github.com/arindam69x/Shukun-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
