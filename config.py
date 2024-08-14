531224cf9e9c236d5d7da90ae41b567636dbdc5c
arindam69x <147473276+arindam69x@users.noreply.github.com>
2024-08-14 15:01:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,22 +78,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/25cfb0bd7581a0d4aa399.jpg"
+    "START_IMG_URL", "https://telegra.ph//file/260b71399f061c1f0d9fb.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/138a639303d54900d5407.jpg"
+    "PING_IMG_URL", "https://telegra.ph//file/2d576fcf982ff294b0d5a.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/cfa72a62efa7e728197f6.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/f0c934035d3c928334a14.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/504de8bd60108a6c188e8.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/504de8bd60108a6c188e8.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph//file/77ba78d195c2e26de2d32.jpg"
+STATS_IMG_URL = "https://telegra.ph//file/e9abb075cdc98091fd73b.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph//file/a64b601cce0f45a192ce1.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph//file/37f24422832e727ebf478.jpg"
 STREAM_IMG_URL = "https://telegra.ph//file/7c896988bd3d0b91b8e46.jpg"
 SOUNCLOUD_IMG_URL = "https://telegra.ph//file/1df85370be3f13663a2d9.jpg"
 YOUTUBE_IMG_URL = "https://telegra.ph//file/38124ca6dea236fb7e4ef.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph//file/adf5178b6e62a11b2c402.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph//file/9a258a9e3c9c3b6f61169.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph//file/b3b2e9a46166f3ee1a5cf.jpg"
-
 
 def time_to_seconds(time):
     stringt = str(time)
